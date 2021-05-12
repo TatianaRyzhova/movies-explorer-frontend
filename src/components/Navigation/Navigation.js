254d2fa86f0e0933b140139cc11cead3457d0b7b
Tatiana Ryzhova <tatiana.ryzhova@deliveryhero.com>
2021-05-12 22:11:35 +0200

    add navigation layout when user is logged in

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link, Route, Switch} from 'react-router-dom';
+import accountIcon from "../../images/account-icon.svg"
 
 function Navigation() {
   return (
@@ -7,15 +8,38 @@ function Navigation() {
     <Switch>
       <nav className="navigation">
 
-        <Route path="/">
-          <Link to="/signup" className="navigation__signup">Register</Link>
-        </Route>
+        <div className="navigation__not-logged">
+          <Route path="/">
+            <Link to="/signup" className="navigation__signup">Register</Link>
+          </Route>
 
-        <Route path="/">
-          <Link to="/signin" className="header__signin">
-            <button className="navigation__signin-button" type='button' aria-label='menu'>Sign In</button>
-          </Link>
-        </Route>
+          <Route path="/">
+            <Link to="/signin" className="header__signin">
+              <button className="navigation__signin-button" type='button' aria-label='menu'>Sign In</button>
+            </Link>
+          </Route>
+        </div>
+
+        <div className="navigation__logged">
+          <div className="navigation__movies-container">
+            <Route path="/">
+              <Link to="/movies" className="navigation__movies">Movies</Link>
+            </Route>
+            <Route path="/">
+              <Link to="/saved-movies" className="navigation__movies">Saved Movies</Link>
+            </Route>
+          </div>
+          <div>
+            <Route path="/">
+              <Link to="/profile" className="">
+                <button className="navigation__profile-button" type='button' aria-label='menu'>
+                  <img src={accountIcon} alt="account icon" className="navigation__profile-icon"/>
+                  Account
+                </button>
+              </Link>
+            </Route>
+          </div>
+        </div>
 
       </nav>
     </Switch>
