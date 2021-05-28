0f24392b41750dc022b67f1726dac5a9c0312774
Tatiana Ryzhova <tatiana.ryzhova@deliveryhero.com>
2021-05-28 21:15:24 +0200

    use a valid urls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,4 @@
-// export const BASE_URL = 'http://api.movies.tatiana.nomoredomains.icu';
-// export const AUTH_BASE_URL = 'http://api.movies.tatiana.nomoredomains.icu';
+export const BASE_URL = 'http://api.movies.tatiana.nomoredomains.icu';
+export const AUTH_BASE_URL = 'http://api.movies.tatiana.nomoredomains.icu';
 export const MOVIES_BASE_URL = 'https://api.nomoreparties.co';
-
-export const BASE_URL = 'http://localhost:3000';
-export const AUTH_BASE_URL = 'http://localhost:3000';
-
 export const DEFAULT_TRAILER_LINK ='https://www.youtube.com/watch?v=5qap5aO4i9A';
